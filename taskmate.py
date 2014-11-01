b5c17fea4cce24ca83b01d284d290a02b45b0429
agomez14 <agomez14@illinois.edu>
2014-11-01 03:21:01 -0400

    Added Yo send requests

--- taskmate.py
+++ taskmate.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
+YO_API_TOKEN = 'b0651284-6164-485d-99ee-fa95a8f4f13a'
 
 @app.route('/') 
 def index():
@@ -17,6 +19,21 @@ def create():
     
 	return render_template('create.html', values=template_values)
 
+@app.route('/yo')
+def yo():
+	template_values = {}
+	return render_template('yo.html', values=template_values)
+
+@app.route('/yo/send')
+def yo_request():
+	username = request.args.get('username')
+	template_values = {'username':username,}
+	requests.post("http://api.justyo.co/yo/", data={'api_token': YO_API_TOKEN,'username':username})
+	return render_template('yo.html',values=template_values)
+
+
+
+	return render_template
 if __name__ == "__main__":
 
 	app.run(debug=True)
