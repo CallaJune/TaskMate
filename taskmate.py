4b24cb4249b9fd399e4f4b477f2b3f588c67e00c
Calla Carter <maracondo@aol.com>
2014-11-01 04:41:24 -0400

    oauth test

--- taskmate.py
+++ taskmate.py
@@ -1,13 +1,37 @@
 from flask import Flask, render_template, request
 import requests
-
+from flask_oauthlib.client import OAuth
 app = Flask(__name__)
 
 YO_API_TOKEN = 'b0651284-6164-485d-99ee-fa95a8f4f13a'
 
+oauth = OAuth()
+tasks = oauth.remote_app('tasks',
+    consumer_key='805075416684-sm8ktm9sel14r7fv42tocdmpsk423fni.apps.googleusercontent.com',
+    consumer_secret='p_4wg243g0N5seeG9sgxj1fz',request_token_params={'scope':['https://www.googleapis.com/auth/tasks']},
+    base_url='https://www.googleapis.com/oauth2/v1',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    access_token_method='POST',
+    access_token_url='https://accounts.google.com/o/oauth2/token', 
+    request_token_url=None
+)
+
 @app.route('/') 
 def index():
    return render_template('home.html')
+
+@app.route('/login')
+def login():
+	print tasks.authorize(callback=url_for('authorized',_external=True))
+	return tasks.authorize(callback=url_for('authorized',_external=True))
+
+@app.route('/login/authorized')
+@tasks.authorized_handler
+def authorized(resp):
+	session['tasks_token'] = (resp['access_token'])
+	user = tasks.get('userinfo')
+	session['user_email'] = user.data['email']
+	return user
 
 @app.route('/create') 
 def create():
