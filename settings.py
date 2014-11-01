dcd2ecad1f65a7cfe05a101bd4dc2d2cf86d66fc
Calla Carter <maracondo@aol.com>
2014-11-01 17:38:13 -0400

    Running i:wq t

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
-CLIENT_ID =  ''
-CLIENT_SECRET = ''
+CLIENT_ID =  '805075416684-sm8ktm9sel14r7fv42tocdmpsk423fni.apps.googleusercontent.com'
+CLIENT_SECRET = 'p_4wg243g0N5seeG9sgxj1fz'
 SCOPE=['https://www.googleapis.com/auth/tasks','https://www.googleapis.com/auth/tasks.readonly','https://www.googleapis.com/auth/tasks']
