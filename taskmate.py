eb40c14c78339ad85e77b6f796048f84e8895536
Calla Carter <maracondo@aol.com>
2014-11-01 03:48:45 -0400

    With yo

--- taskmate.py
+++ taskmate.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
+
 YO_API_TOKEN = 'b0651284-6164-485d-99ee-fa95a8f4f13a'
 
 @app.route('/') 
@@ -27,13 +28,9 @@ def yo():
 @app.route('/yo/send')
 def yo_request():
 	username = request.args.get('username')
-	template_values = {'username':username,}
+	template_values = {'username':username}
 	requests.post("http://api.justyo.co/yo/", data={'api_token': YO_API_TOKEN,'username':username})
-	return render_template('yo.html',values=template_values)
+	return render_template('yo.html', values=template_values)
 
-
-
-	return render_template
 if __name__ == "__main__":
-
 	app.run(debug=True)
