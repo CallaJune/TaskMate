9c042171cf22093f940094b4d76fa744a7dbba39
Calla Carter <maracondo@aol.com>
2014-11-01 01:16:06 -0400

    Basic passing of values

--- taskmate.py
+++ taskmate.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/') 
 def index():
-   
-   return "Hello"
+   return render_template('home.html')
 
+@app.route('/create') 
+def create():
+	template_values = {}
+	eventname = request.args.get('eventname')
+	eventdescription = request.args.get('eventdescription')
+
+	template_values['eventname'] = eventname
+	template_values['eventdescription'] = eventdescription
+    
+	return render_template('create.html', values=template_values)
+
+if __name__ == "__main__":
+
+	app.run(debug=True)
