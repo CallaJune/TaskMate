c9e9c21080f5a48e5322008269ea9fb0ea96f294
Calla Carter <maracondo@aol.com>
2014-11-01 21:41:46 -0400

    User can create a task

--- main.py
+++ main.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import webapp2
+import datetime
 from webapp2_extras import jinja2
 
 from apiclient.discovery import build
@@ -49,10 +50,67 @@ class MainHandler(webapp2.RequestHandler):
 def truncate(s, l):
   return s[:l] + '...' if len(s) > l else s
 
+class NewHandler(webapp2.RequestHandler):
+  def render_response(self, template, **context):
+    renderer = jinja2.get_jinja2(app=self.app)
+    rendered_value = renderer.render_template(template, **context)
+    self.response.write(rendered_value)
 
+  @decorator.oauth_aware
+  def get(self):
+    if decorator.has_credentials():
+      result = service.tasks().list(tasklist='@default').execute(
+          http=decorator.http())
+      tasks = result.get('items', [])
+      for task in tasks:
+        task['title_short'] = truncate(task['title'], 20)
+      self.render_response('new.html', tasks=tasks)
+    else:
+      url = decorator.authorize_url()
+      self.render_response('new.html', tasks=[], authorize_url=url)
+
+class CreateHandler(webapp2.RequestHandler):
+  def render_response(self, template, **context):
+    renderer = jinja2.get_jinja2(app=self.app)
+    rendered_value = renderer.render_template(template, **context)
+    self.response.write(rendered_value)
+
+  @decorator.oauth_aware
+  def get(self):  
+        template_values = {
+        }
+        taskname = self.request.get('taskname')
+        taskdescription = self.request.get('taskdescription')
+
+        #transfer taskdue into 2014-11-03T12:00:00.000Z format
+        pretaskdue = self.request.get('taskdue')
+        taskdue = pretaskdue[:10] + 'T' + pretaskdue[11:] + ':00.000Z'
+
+        template_values['taskname'] = taskname
+        template_values['taskdescription'] = taskdescription
+        template_values['taskdue'] = taskdue
+
+        #create a task with all that information and do everything with yo
+        tasks = {
+          'title': taskname,
+          'notes': taskdescription,
+          'due': taskdue
+          }
+        tasks = service.tasks().insert(tasklist='@default', body=tasks).execute(http=decorator.http())
+        print tasks['id']
+
+        '''
+        1. Figure out how to insert a task. 
+        2. Confirm due date format.
+        3. Request due date from user.
+        '''
+
+        self.redirect('/')
 
 routes = [
 		webapp2.Route('/', MainHandler, name='home'),
+    webapp2.Route('/new', NewHandler, name='new'),
+    webapp2.Route('/create', CreateHandler, name='create'),
 		webapp2.Route(decorator.callback_path, decorator.callback_handler(), name='callback'),
 		]
 
