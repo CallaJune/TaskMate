7f0836b3f0273c61aea211560f5f3e5f0b9e6cbf
agomez14 <agomez14@illinois.edu>
2014-11-01 21:55:58 -0400

    Added some stuff to gitignore

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 CLIENT_ID =  ''
 CLIENT_SECRET = ''
-SCOPE=['https://www.googleapis.com/auth/tasks','https://www.googleapis.com/auth/tasks.readonly','https://www.googleapis.com/auth/tasks']
+SCOPE=''
 YO_API_TOKEN= ''
 
